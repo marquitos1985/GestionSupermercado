5f40be28176c8f6003067f19e3a1233fd281d9f0
“lucasRiggillo” <lucasRiggillo>
2024-05-25 20:53:39 -0300

    Se añadio gestor y funcionalidades al vendedor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import usuarios.empleados.administrador.GestorAdministrador;
+import usuarios.empleados.vendedor.GestorVendedores;
 
 public class Main {
     public static void main(String[] args) {
-        GestorAdministrador gestorAdministrador = new GestorAdministrador();
-        gestorAdministrador.crearUnAdministrador("Nicolas Blanco Rio", 42597483, "Beltrami 1560","2233060608",true,"nicolasuxmal@gmail.com","lapulga", 60000F);
-        gestorAdministrador.guardarArchivoJsonAdministradores();
+        GestorVendedores gestorVendedores = new GestorVendedores();
+        gestorVendedores.crearUnVendedor("Nicolas Blanco Rio", 42597483, "Beltrami 1560","2233060608",true,"nicolasuxmal@gmail.com","lapulga", 60000F);
+        gestorVendedores.crearUnVendedor("Lucas Riggillo", 44692854, "aguado 1601","2235403203",true,"lucasriggillo@gmail.com","hola", 60000F);
+        gestorVendedores.modificarDireccionVendedor(44692854,"tripulantes 2802");
+        gestorVendedores.guardarArchivoJsonVendedores();
     }
 }
